69080dbe83f7464752e35ddd8e4e9955fb0df634
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-26 19:59:27 +0500

    Initialize server and app setup with Express and dotenv

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+
+
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+
+
+
+
+
+module.exports = app
