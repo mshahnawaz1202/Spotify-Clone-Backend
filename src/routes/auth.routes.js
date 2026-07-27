6444b571ff88eb4f124dd5d39d77bfa3355bb33f
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-27 11:48:16 +0500

    Fix registration route to properly link to user registration controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,8 @@
 const express = require('express')
-
+const authController = require('../controllers/auth.controller')
 const router = express.Router()
 
-router.post('/register',)
+router.post('/register',authController.registerUser)
 
 
 
