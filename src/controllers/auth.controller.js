9c053aa740ad524d3e9c249fe81c21476fa021cc
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-27 12:18:46 +0500

    Fix typo in cookie setting and implement login functionality with JWT token generation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -29,7 +29,7 @@ async function registerUser(req, res) {
         role: user.role
     }, process.env.JWT_SECRET)
 
-    res.cookies('token', token)
+    res.cookie('token', token)
 
     res.status(201).json({
         message: "User Created Successfully!",
@@ -46,7 +46,56 @@ async function registerUser(req, res) {
 
 }
 
-module.exports = { registerUser }
+
+async function loginUser(req,res){
+
+    const {username,email,password} = req.body
+
+    const user = await userModel.findOne({
+        $or:[
+            {username},
+            {email},
+        ]
+    })
+
+    if(!user){
+        return res.status(401).json({
+            message : "Invalid Credentials"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password,user.password)
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message : "Invalid Password"
+        })
+    }
+
+    const token = jwt.sign({
+        id:user._id,
+        role:user.role
+    },process.env.JWT_SECRET)
+
+    res.cookie('token',token)
+
+    res.status(201).json({
+        message: "User Logged In Successfully!",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+    })
+
+
+
+
+
+}
+
+
+module.exports = { registerUser,loginUser }
 
 
 
