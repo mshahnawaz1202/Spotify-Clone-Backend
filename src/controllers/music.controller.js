2bf4c383f5e2a92dba80aba8c9d1d5446fe9c521
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-27 17:36:13 +0500

    Refactor createMusic function to improve variable scope and error handling

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -13,43 +13,50 @@ async function createMusic(req, res) {
         })
     }
 
-    let decoded;
+
     try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         if (decoded.role !== "artist") {
             return res.status(403).json({
                 message: "Forbidden\n You don't have access to create music"
             })
         }
 
-    }
-    catch (err) {
-        return res.status(401).json({
-            message: "Unauthorized!"
+
+
+
+        const { title } = req.body
+        const file = req.file
+
+        const result = await uploadFile(
+            file.buffer,
+            file.originalname
+        );
+        console.log(result);
+
+        const music = await musicModel.create({
+            uri: result.url,
+            title,
+            artist: decoded.id
         })
 
+        res.status(201).json({
+            message: 'Music created successfully',
+            music: {
+                id: music._id,
+                uri: music.uri,
+                title: music.title,
+                artist: music.artist,
+            },
+        });
     }
+    catch (err) {
+        console.error(err);
 
-    const { title } = req.body
-    const file = req.file
-
-    const result = await uploadFile(file.buffer.toString('base64'))
-
-    const music = await musicModel.create({
-        uri: result.uri,
-        title,
-        artist: decoded.id
-    })
-
-    res.status(201).json({
-        message: 'Music created successfully',
-        music: {
-            id: music._id,
-            uri: music.uri,
-            title: music.title,
-            artist: music.artist,
-        },
-    });
+        return res.status(401).json({
+            message: err.message
+        });
+    }
 
 
 }
