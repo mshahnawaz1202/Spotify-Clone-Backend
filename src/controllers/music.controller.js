f19f7cc29e94a12eac1ea3e3abb40e991971a1ae
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-27 18:18:07 +0500

    Add createAlbum function to handle music album creation

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,9 +1,10 @@
 const musicModel = require('../models/music.model')
 const jwt = require('jsonwebtoken')
 const { uploadFile } = require('../services/storage.service')
+const albumModel = require('../models/album.model')
 
-
-
+/**--------------------------------------------------------------------------------------------------------------- */
+/** function to create music */
 async function createMusic(req, res) {
 
     const token = req.cookies.token
@@ -60,7 +61,51 @@ async function createMusic(req, res) {
 
 
 }
+/**--------------------------------------------------------------------------------------------------------------- */
+/** function to create music album */
+async function createAlbum(req,res){
+    const token = req.cookies.token
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
+    }
 
-module.exports = { createMusic }
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if (decoded.role !== "artist") {
+            return res.status(403).json({
+                message: "Forbidden\n You don't have access to create music"
+            })
+        }
+
+        const {title,musicIds} = req.body
+
+        const album = await albumModel.create({
+            title,
+            artist:decoded.id,
+            musics:musicIds
+        })
+        res.status(201).json({
+            message: 'Music Album created successfully',
+            music: {
+               id:album._id,
+               title:album.title,
+               artist:album.artist,
+               musics:album.musics
+            },
+        });
+
+    }catch(err){
+        console.error(err);
+
+        return res.status(401).json({
+            message: err.message
+        });
+        
+    }
+}
+
+module.exports = { createMusic,createAlbum }
 
 
