676c5d851a61a2f9c2bac2a9cd3839aaaee3e209
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-27 18:40:53 +0500

    Add authentication middleware for artist role verification

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+
+
+async function authArtist(req, res, next) {
+    const token = req.cookies.token
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if (decoded.role !== "artist") {
+            return res.status(403).json({
+                message: "Forbidden\n You don't have access to create music"
+            })
+        }
+
+        next() //in routes moving request to next 
+        
+
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
+    }
+}
+
+module.exports = {
+    authArtist
+};
