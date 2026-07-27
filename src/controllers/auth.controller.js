831327181a652137c6f748aec636219b49805cb3
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-27 11:28:51 +0500

    Implement user registration logic with JWT token generation

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,51 @@
+const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+
+async function registerUser(req, res) {
+    const { username, email, password, role = "user" } = req.body
+
+    const isUserExists = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+
+    if (isUserExists) {
+        return res.status(400).json({
+            message: "User Already exists"
+        })
+    }
+
+    const user = await userModel.create({
+        username, email, password, role
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role
+    }, process.env.JWT_SECRET)
+
+    res.cookies('token', token)
+
+    res.status(201).json({
+        message: "User Created Successfully!",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+    })
+
+
+
+
+}
+
+
+
+
+
+
+
