b6bc870cfe810fb37ac314b99538f57317531f50
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-27 16:26:14 +0500

    Refactor createMusic function for improved readability and consistency

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,38 +1,59 @@
 const musicModel = require('../models/music.model')
 const jwt = require('jsonwebtoken')
+const { uploadFile } = require('../services/storage.service')
 
 
-async function createMusic (req,res) {
+
+async function createMusic(req, res) {
 
     const token = req.cookies.token
-    if(!token){
+    if (!token) {
         return res.status(401).json({
             message: "Unauthorized!"
         })
     }
 
-    
-    try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET)
-        if(decoded.role !== "artist"){
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if (decoded.role !== "artist") {
             return res.status(403).json({
-            message: "Forbidden\n You don't have access to create music"
-        })
+                message: "Forbidden\n You don't have access to create music"
+            })
         }
-        
+
     }
-    catch(err){
+    catch (err) {
         return res.status(401).json({
             message: "Unauthorized!"
         })
 
     }
 
-    const{title} = req.body
+    const { title } = req.body
     const file = req.file
 
+    const result = await uploadFile(file.buffer.toString('base64'))
+
+    const music = await musicModel.create({
+        uri: result.uri,
+        title,
+        artist: decoded.id
+    })
+
+    res.status(201).json({
+        message: 'Music created successfully',
+        music: {
+            id: music._id,
+            uri: music.uri,
+            title: music.title,
+            artist: music.artist,
+        },
+    });
 
 
 }
+
+module.exports = { createMusic }
 
 
