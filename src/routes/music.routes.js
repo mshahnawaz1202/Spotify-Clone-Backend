64029d06a7c6ee816f778f6d6ba8433a7cdc1588
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-27 17:36:27 +0500

    Add multer configuration for file upload handling in music routes

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,7 +1,13 @@
 const express = require('express')
-
+const musicConroller = require('../controllers/music.controller')
 const router = express.Router()
+const multer = require("multer");
 
+const upload = multer({
+    storage:multer.memoryStorage()
+})
+
+router.post("/upload", upload.single("music") ,musicConroller.createMusic);
 
 
 
